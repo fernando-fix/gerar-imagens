ed93a55c42ffb3bb01e7d2884f26202c998c7775
Fernando Rodrigues <fernando_tecti@hotmail.com>
2023-01-01 16:04:57 -0300

    mobile working when the range is changing

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,30 +5,27 @@ let h_width = document.querySelector("#h_width"); //select input number
 let r_height = document.querySelector("#r_height"); //select range
 let h_height = document.querySelector("#h_height"); //select input number
 
-//events
+//change the range input when input box value changes
 h_width.addEventListener('change', function () {
     r_width.value = h_width.value;
 });
-
-r_width.addEventListener('mousemove', function () {
-    h_width.value = r_width.value;
-});
-
 h_height.addEventListener('change', function () {
     r_height.value = h_height.value;
 });
 
+//change the input box when range input is moving
+r_width.addEventListener('mousemove', function () {
+    h_width.value = r_width.value;
+});
 r_height.addEventListener('mousemove', function () {
     h_height.value = r_height.value;
 });
 
-
-//for cellphone and tablet
-r_width.addEventListener('change', function () {
+//change the input box when range input is moving on cellphone and tablet
+r_width.addEventListener('touchmove', function () {
     h_width.value = r_width.value;
 });
-
-r_height.addEventListener('change', function () {
+r_height.addEventListener('touchmove', function () {
     h_height.value = r_height.value;
 });
 
